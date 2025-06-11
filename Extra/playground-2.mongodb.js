d66188fbbaafb3766a7d41a1f49d2336abf781c8
Luis Rodrigo - Net <luis.rodrigo.net@gmail.com>
2025-06-11 15:44:08 -0300

    Extra

---
+++ Extra/playground-2.mongodb.js
@@ -0,0 +1,11 @@
+/* global use, db */
+// MongoDB Playground
+// Use Ctrl+Space inside a snippet or a string literal to trigger completions.
+
+const database = 'desafio05';
+
+// The current database to use.
+use(database);
+
+
+db.getCollectionNames()
